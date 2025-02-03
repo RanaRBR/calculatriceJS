8ea0a9ef64ddab5ef0b42e350e86e1c41294da7a
RanaRBR <boutrosrafaelrana@gmail.com>
2025-02-03 17:36:43 +0100

    suite2

--- public/js/main.js
+++ public/js/main.js
@@ -1,38 +1,26 @@
-let monChamp = document.getElementById('champ')
+
+let monChamp = document.getElementById('champ');
 
 function ajout(input) {
     if (monChamp.value === '0') {
-        monChamp.value = input
+        monChamp.value = input;
     } else {
-        monChamp.value += input
+        monChamp.value += input;
     }
 }
 
-function clearDisplay() {
+function suppression() {
     monChamp.value = '0'
 }
 
 
+function suppressionDernier() {
+    if (monChamp.value.length > 1) {
+        monChamp.value = monChamp.value.slice(0, -1);
+    } else {
+        monChamp.value = '0';
+    }
+}
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// let boutons = document.querySelectorAll("button");
-
-// boutons.addventLitsener("click", function(){
-//     boutons.style.transform 
-
-// })
